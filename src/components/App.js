1bd38076780fca00968f2ad4897f0f7bb968f565
GauraviThakur <107761300+GauraviThakur@users.noreply.github.com>
2022-07-31 18:11:15 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../styles/App.css';
+const App = () => {
+
+  const handleClick = (event) =>{
+    // use console.log
+
+  }
+
+  // do not remove the two buttons or change their id
+  return (
+    <div id="main">
+      <button id="button-a">Button A</button>
+      <button id="button-b">Button B</button>
+    </div>
+  )
+}
+
+
+export default App;
